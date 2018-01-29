cbcb9b74ccfd888b6dd1fcaf61f5a83e125701c1
Will <will@glozer.net>
2018-01-29 16:59:58 +0900

    support new FPEX_LATENCY_MS column

--- src/github.com/kentik/libkflow/cmd/server/main.go
+++ src/github.com/kentik/libkflow/cmd/server/main.go
@@ -11,18 +11,19 @@ import (
 )
 
 type Args struct {
-	Port       int          `short:"p"          description:"listen on port "`
-	Host       string       `long:"host"        description:"listen on host "`
-	TLS        bool         `long:"tls"         description:"require TLS    "`
-	Quiet      bool         `long:"quiet"       description:"minimize output"`
-	Email      string       `long:"email"       description:"API auth email "`
-	Token      string       `long:"token"       description:"API auth token "`
-	CompanyID  int          `long:"company-id"  description:"company ID     "`
-	DeviceID   int          `long:"device-id"   description:"device ID      "`
-	DeviceName string       `long:"device-name" description:"device name    "`
-	DeviceIP   string       `long:"device-ip"   description:"device IP addr "`
-	MaxFPS     int          `long:"max-fps"     description:"max flows/sec  "`
-	Customs    []api.Column `long:"custom"      description:"custom fields  "`
+	Port       int          `short:"p"          description:"listen on port    "`
+	Host       string       `long:"host"        description:"listen on host    "`
+	TLS        bool         `long:"tls"         description:"require TLS       "`
+	Quiet      bool         `long:"quiet"       description:"minimize output   "`
+	Email      string       `long:"email"       description:"API auth email    "`
+	Token      string       `long:"token"       description:"API auth token    "`
+	CompanyID  int          `long:"company-id"  description:"company ID        "`
+	DeviceID   int          `long:"device-id"   description:"device ID         "`
+	DeviceName string       `long:"device-name" description:"device name       "`
+	DeviceIP   string       `long:"device-ip"   description:"device IP addr    "`
+	Sample     int          `long:"sample"      description:"device sample rate"`
+	MaxFPS     int          `long:"max-fps"     description:"max flows/sec     "`
+	Customs    []api.Column `long:"custom"      description:"custom fields     "`
 }
 
 func main() {
@@ -37,6 +38,7 @@ func main() {
 		DeviceID:   1,
 		DeviceName: api.NormalizeName("dev1"),
 		DeviceIP:   "127.0.0.1",
+		Sample:     1,
 		MaxFPS:     4000,
 		Customs: []api.Column{
 			{ID: 1, Type: "uint32", Name: "RETRANSMITTED_IN_PKTS"},
@@ -45,36 +47,37 @@ func main() {
 			{ID: 4, Type: "uint32", Name: "CLIENT_NW_LATENCY_MS"},
 			{ID: 5, Type: "uint32", Name: "SERVER_NW_LATENCY_MS"},
 			{ID: 6, Type: "uint32", Name: "APPL_LATENCY_MS"},
-			{ID: 7, Type: "uint32", Name: "OOORDER_IN_PKTS"},
-			{ID: 8, Type: "uint32", Name: "OOORDER_OUT_PKTS"},
-			{ID: 9, Type: "string", Name: "KFLOW_HTTP_URL"},
-			{ID: 10, Type: "uint32", Name: "KFLOW_HTTP_STATUS"},
-			{ID: 11, Type: "string", Name: "KFLOW_HTTP_UA"},
-			{ID: 12, Type: "string", Name: "KFLOW_HTTP_REFERER"},
-			{ID: 13, Type: "string", Name: "KFLOW_HTTP_HOST"},
-			{ID: 14, Type: "string", Name: "KFLOW_DNS_QUERY"},
-			{ID: 15, Type: "uint32", Name: "KFLOW_DNS_QUERY_TYPE"},
-			{ID: 16, Type: "uint32", Name: "KFLOW_DNS_RET_CODE"},
-			{ID: 17, Type: "string", Name: "KFLOW_DNS_RESPONSE"},
-			{ID: 18, Type: "uint32", Name: "REPEATED_RETRANSMITS"},
-			{ID: 19, Type: "uint32", Name: "RECEIVE_WINDOW"},
-			{ID: 20, Type: "uint32", Name: "ZERO_WINDOWS"},
-			{ID: 21, Type: "uint32", Name: "CONNECTION_ID"},
-			{ID: 22, Type: "uint32", Name: "OOORDER_PKTS"},
-			{ID: 23, Type: "uint32", Name: "RETRANSMITTED_PKTS"},
-			{ID: 24, Type: "uint32", Name: "APP_PROTOCOL"},
-			{ID: 25, Type: "uint32", Name: "INT00"},
-			{ID: 26, Type: "uint32", Name: "INT01"},
-			{ID: 27, Type: "uint32", Name: "INT02"},
-			{ID: 28, Type: "uint32", Name: "INT03"},
-			{ID: 29, Type: "uint32", Name: "INT04"},
-			{ID: 30, Type: "uint32", Name: "INT05"},
-			{ID: 31, Type: "string", Name: "STR00"},
-			{ID: 32, Type: "string", Name: "STR01"},
-			{ID: 33, Type: "string", Name: "STR02"},
-			{ID: 34, Type: "string", Name: "STR03"},
-			{ID: 35, Type: "string", Name: "STR04"},
-			{ID: 36, Type: "string", Name: "STR05"},
+			{ID: 7, Type: "uint32", Name: "FPEX_LATENCY_MS"},
+			{ID: 8, Type: "uint32", Name: "OOORDER_IN_PKTS"},
+			{ID: 9, Type: "uint32", Name: "OOORDER_OUT_PKTS"},
+			{ID: 10, Type: "string", Name: "KFLOW_HTTP_URL"},
+			{ID: 11, Type: "uint32", Name: "KFLOW_HTTP_STATUS"},
+			{ID: 12, Type: "string", Name: "KFLOW_HTTP_UA"},
+			{ID: 13, Type: "string", Name: "KFLOW_HTTP_REFERER"},
+			{ID: 14, Type: "string", Name: "KFLOW_HTTP_HOST"},
+			{ID: 15, Type: "string", Name: "KFLOW_DNS_QUERY"},
+			{ID: 16, Type: "uint32", Name: "KFLOW_DNS_QUERY_TYPE"},
+			{ID: 17, Type: "uint32", Name: "KFLOW_DNS_RET_CODE"},
+			{ID: 18, Type: "string", Name: "KFLOW_DNS_RESPONSE"},
+			{ID: 19, Type: "uint32", Name: "REPEATED_RETRANSMITS"},
+			{ID: 20, Type: "uint32", Name: "RECEIVE_WINDOW"},
+			{ID: 21, Type: "uint32", Name: "ZERO_WINDOWS"},
+			{ID: 22, Type: "uint32", Name: "CONNECTION_ID"},
+			{ID: 23, Type: "uint32", Name: "OOORDER_PKTS"},
+			{ID: 24, Type: "uint32", Name: "RETRANSMITTED_PKTS"},
+			{ID: 25, Type: "uint32", Name: "APP_PROTOCOL"},
+			{ID: 26, Type: "uint32", Name: "INT00"},
+			{ID: 27, Type: "uint32", Name: "INT01"},
+			{ID: 28, Type: "uint32", Name: "INT02"},
+			{ID: 29, Type: "uint32", Name: "INT03"},
+			{ID: 30, Type: "uint32", Name: "INT04"},
+			{ID: 31, Type: "uint32", Name: "INT05"},
+			{ID: 32, Type: "string", Name: "STR00"},
+			{ID: 33, Type: "string", Name: "STR01"},
+			{ID: 34, Type: "string", Name: "STR02"},
+			{ID: 35, Type: "string", Name: "STR03"},
+			{ID: 36, Type: "string", Name: "STR04"},
+			{ID: 37, Type: "string", Name: "STR05"},
 		},
 	}
 
@@ -100,6 +103,7 @@ func main() {
 		ID:          args.DeviceID,
 		Name:        args.DeviceName,
 		IP:          net.ParseIP(args.DeviceIP),
+		SampleRate:  args.Sample,
 		MaxFlowRate: args.MaxFPS,
 		CompanyID:   args.CompanyID,
 		Customs:     args.Customs,
